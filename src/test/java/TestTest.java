15b7870701f6eae52f58cc3865019abb49478af1
Niels Bokmans <NJC.Bokmans@student.han.nl>
2016-04-12 15:34:52 +0200

    Add test folder

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Niels Bokmans on 12-4-2016.
+ */
+public class TestTest {
+    @Test
+    public void doTest() {
+        assertEquals(1, 1);
+    }
+}
