0e7d7c2fae97542e001c6cba1fbf5d8cdb0c1016
Marius Linders <pm.linders@student.han.nl>
2016-04-19 14:11:20 +0200

    Fixed and cleaned the tests. Added a simple GUI for registration.

---
+++ src/main/java/nl/han/asd/project/client/commonclient/presentation/GUI.java
@@ -0,0 +1,18 @@
+package nl.han.asd.project.client.commonclient.presentation;/**
+ * Created by Marius on 19-04-16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
