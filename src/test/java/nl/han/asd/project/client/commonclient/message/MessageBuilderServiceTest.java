75b1c74ff2110a8495b135162110bc1ffcda8592
Niels Bokmans <NJC.Bokmans@student.han.nl>
2016-05-13 23:05:24 +0200

    Socket was taking up to 2 minutes to wait for a connection. Jenkins build takes too long that way. Set timeout to 7 seconds.

--- src/test/java/nl/han/asd/project/client/commonclient/message/MessageBuilderServiceTest.java
+++ src/test/java/nl/han/asd/project/client/commonclient/message/MessageBuilderServiceTest.java
@@ -50,18 +50,17 @@ public class MessageBuilderServiceTest {
 
     @Test
     public void sendMessageTest(){
+        //TODO: fix this test that takes ridiculously long to execute and doesn't even test anything
         Contact contactReciever = new Contact("julius","1234");
         Contact contactSender = new Contact("bram","123456");
         contactReciever.setConnectedNodes(new Node[]{new Node("NODE_ID_1","192.168.2.8",1234,"123456789".getBytes()),new Node("NODE_ID_2","192.168.2.9",1234,"123456789".getBytes()),new Node("NODE_ID_3","192.168.2.10",1234,"123456789".getBytes())});
-
         ArrayList<Node> path = new ArrayList<Node>(Arrays.asList(new Node("NODE_ID_1","192.168.2.1",1234,"123456789".getBytes()), new Node("NODE_ID_2","192.168.2.2",1234,"123456789".getBytes()), new Node("NODE_ID_3","192.168.2.3",1234,"123456789".getBytes())));
         Byte[] encryptedData = new Byte[]{0,1,0,1};
-
         Mockito.when(pathDeterminationService.getPath(anyInt(),any(Contact.class))).thenReturn(path);
         Mockito.when(encrypt.encryptData(Mockito.any(ByteString.class),Mockito.any(byte[].class))).thenReturn(ByteString.copyFromUtf8("data"));
         Mockito.when(pathDeterminationService.getPath(anyInt(),any(Contact.class))).thenReturn(path);
         messageBuilderService.sendMessage("hallo 124",contactReciever,contactSender);
-	}
+    }
 
 
 
