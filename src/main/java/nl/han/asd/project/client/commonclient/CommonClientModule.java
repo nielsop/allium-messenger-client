f1260049544ec76d8797bb46555d2e6b25cc1b16
RaoulMeyer <raouljmeyer@gmail.com>
2016-05-31 14:24:57 +0200

    Added CommonClientModule

---
+++ src/main/java/nl/han/asd/project/client/commonclient/CommonClientModule.java
@@ -0,0 +1,35 @@
+package nl.han.asd.project.client.commonclient;
+
+import com.google.inject.AbstractModule;
+import nl.han.asd.project.client.commonclient.connection.ConnectionModule;
+import nl.han.asd.project.client.commonclient.database.DatabaseModule;
+import nl.han.asd.project.client.commonclient.graph.GraphModule;
+import nl.han.asd.project.client.commonclient.heartbeat.HeartbeatModule;
+import nl.han.asd.project.client.commonclient.login.LoginModule;
+import nl.han.asd.project.client.commonclient.master.MasterModule;
+import nl.han.asd.project.client.commonclient.message.MessageModule;
+import nl.han.asd.project.client.commonclient.node.NodeModule;
+import nl.han.asd.project.client.commonclient.path.PathModule;
+import nl.han.asd.project.client.commonclient.persistence.PersistenceModule;
+import nl.han.asd.project.client.commonclient.scripting.ScriptModule;
+import nl.han.asd.project.client.commonclient.store.StoreModule;
+import nl.han.asd.project.commonservices.encryption.EncryptionModule;
+
+public class CommonClientModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        install(new ConnectionModule());
+        install(new DatabaseModule());
+        install(new GraphModule());
+        install(new HeartbeatModule());
+        install(new LoginModule());
+        install(new MasterModule());
+        install(new MessageModule());
+        install(new NodeModule());
+        install(new PathModule());
+        install(new PersistenceModule());
+        install(new ScriptModule());
+        install(new StoreModule());
+        install(new EncryptionModule());
+    }
+}
